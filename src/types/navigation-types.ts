056bc94d17139cfb1151cc5e036f544d6c884650
Emre Samurlu <emrsmrl@gmail.com>
2024-09-08 01:34:56 +0300

    feat: update navigation types

--- src/types/navigation-types.ts
+++ src/types/navigation-types.ts
@@ -1,3 +1,24 @@
-export interface RootStackParams {
+import {RouteProp} from '@react-navigation/native';
+import {Pages} from '../constants/pages.enum';
+import {IStoreResponse} from './response-types';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+
+export interface RootTabParams {
   [key: string]: undefined;
 }
+
+export type RootStackParams = {
+  [Pages.ProductList]: undefined;
+  [Pages.ProductDetail]: {product: IStoreResponse};
+  [Pages.Filter]: undefined;
+};
+
+export type ProductScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParams,
+  Pages.ProductDetail
+>;
+
+export type ProductDetailRouteProp = RouteProp<
+  RootStackParams,
+  Pages.ProductDetail
+>;
